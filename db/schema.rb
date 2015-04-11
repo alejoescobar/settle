18ef7689a5f1f677810a56db40be408cd23e9ef6
Alejandro Escobar <alejoescobac@gmail.com>
2015-04-10 20:45:07 -0500

    Add Tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411012632) do
+ActiveRecord::Schema.define(version: 20150411013906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "concepts", force: :cascade do |t|
+    t.string   "concept"
+    t.datetime "date"
+    t.text     "description"
+    t.integer  "difficulty"
+    t.integer  "state"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
