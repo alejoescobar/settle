fc1d3a8871b8485ab3f15324d94cf6ef5807d03b
German Hernandez <muygrafico@gmail.com>
2015-04-11 16:33:58 -0500

    run annotate

--- test/models/concept_test.rb
+++ test/models/concept_test.rb
@@ -10,6 +10,7 @@
 #  state       :integer
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  user_id     :integer
 #
 
 require 'test_helper'
--- test/models/source_test.rb
+++ test/models/source_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: sources
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  url        :string
+#  concept_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class SourceTest < ActiveSupport::TestCase
