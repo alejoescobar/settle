aca604215db19071e9e7df071ac97aaa4d4714be
Alejandro Escobar <alejoescobac@gmail.com>
2015-04-10 21:36:18 -0500

    Add tag model and join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411013906) do
+ActiveRecord::Schema.define(version: 20150411022308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20150411013906) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "concepts_tags", id: false, force: :cascade do |t|
+    t.integer "concept_id", null: false
+    t.integer "tag_id",     null: false
+  end
+
+  add_index "concepts_tags", ["concept_id", "tag_id"], name: "index_concepts_tags_on_concept_id_and_tag_id", using: :btree
+  add_index "concepts_tags", ["tag_id", "concept_id"], name: "index_concepts_tags_on_tag_id_and_concept_id", using: :btree
 
   create_table "tags", force: :cascade do |t|
     t.string   "name"
