3efc5a851dde6601f07dfd2c08a4e7fbde0a8a38
Alejandro Escobar <alejoescobac@gmail.com>
2015-04-13 10:02:34 -0500

    Fix calendar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @concepts = Concept.all.where(user_id: current_user)
-    @concepts_by_date = @concepts.group_by { |concept| concept.created_at.strftime("%F") }
+    @concepts_by_date = @concepts.group_by { |concept| concept.date.strftime("%F") }
   end
 end
